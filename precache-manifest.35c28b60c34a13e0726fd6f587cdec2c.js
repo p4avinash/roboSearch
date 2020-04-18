27bfc8910a0f790e4b9e7b9727a4b5ab09bf59cc
p4avinash <p4avinashmaurya@gmail.com>
2020-04-18 19:11:51 +0530

    Updates

---
+++ precache-manifest.35c28b60c34a13e0726fd6f587cdec2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbe03b4beff47095f08316c67471a9c",
+    "url": "/roboSearch/index.html"
+  },
+  {
+    "revision": "d7d8f6436bfdeef57f0c",
+    "url": "/roboSearch/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "a5dbd89e6c51703c4ee4",
+    "url": "/roboSearch/static/css/main.b8017d25.chunk.css"
+  },
+  {
+    "revision": "d7d8f6436bfdeef57f0c",
+    "url": "/roboSearch/static/js/2.d2460644.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/roboSearch/static/js/2.d2460644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5dbd89e6c51703c4ee4",
+    "url": "/roboSearch/static/js/main.4914be9b.chunk.js"
+  },
+  {
+    "revision": "f0ec05fbd481aaf48341",
+    "url": "/roboSearch/static/js/runtime-main.8495199a.js"
+  }
+]);
